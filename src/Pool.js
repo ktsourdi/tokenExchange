2c05b814cfb5d9e3cff1adf094fa8146971fd656
Kyros Tsourdinis <64464984+ktsourdi@users.noreply.github.com>
2022-10-12 15:40:02 +0300

    Add files via upload

--- src/Pool.js
+++ src/Pool.js
@@ -6,11 +6,11 @@ import Typography from "@material-ui/core/Typography";
 import Button from 'react-bootstrap/Button';
 import Dropdown from 'react-bootstrap/Dropdown';
 import Icon from "react-crypto-icons";
-import dai from './Dai';
 import web3 from './web3';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Exchange from './Exchange.js';
+import _exchange from './ExchangeSol.js';
 import Vote from './Vote.js';
 
 
@@ -24,18 +24,18 @@ class Pool extends Component {
           ethBalance: '0',
           value1: '',
           value2: '',
-          token: 'ETH',
-          token2: 'DAI',
-          rate: [1, 100, 1000], //Μας δείχνει πόσα tokens παίρνει κάποιος με 1 eth (με την σειρά των tokens eth, dai, tsrd)
+          token: 'Επιλέξτε νόμισμα',
+          selectedToken: 0,
           selectedRate1: '',
           selectedRate2: '',
           poolEthDai: [], //πίνακας με διευθύνσεις οι οποίες έχουν κάνει κατάθεση στο pool
           pointerPoolEthDai: [], //πίνακας 
-          stakes: '0',
+          stakes1: '0',
+          stakes2: '0',
+          stakes3: '0'
         };
         this.onChange1 = this.onChange1.bind(this);
-        this.onChange2 = this.onChange2.bind(this);
-        this.showsStake = this.showsStake.bind(this);
+        this.withdrawStake = this.withdrawStake.bind(this);
       }
 
 
@@ -44,70 +44,10 @@ class Pool extends Component {
         if (e.target.value === '' || re.test(e.target.value)) {
            this.setState({value1: e.target.value})
         }
-        
-        if(this.state.token === "ETH" && this.state.token2 === "DAI") {
-          this.setState({value2: e.target.value/this.state.rate[0]*this.state.rate[1]}, 
-            () => {console.log(this.state.value2)}
-            )
-        }
-
-        if(this.state.token === "DAI" && this.state.token2 === "ETH") {
-          this.setState({value2: e.target.value/this.state.rate[1]*this.state.rate[0]}, 
-            () => {console.log(this.state.value2)}
-            )
-        }
-
-        if(this.state.token === "ETH" && this.state.token2 === "TSRD") {
-          this.setState({value2: e.target.value/this.state.rate[0]*this.state.rate[2]}, 
-            () => {console.log(this.state.value2)}
-            )
-        }
-
-        if(this.state.token === "TSRD" && this.state.token2 === "ETH") {
-          this.setState({value2: e.target.value/this.state.rate[2]*this.state.rate[0]}, 
-            () => {console.log(this.state.value2)}
-            )
-        }
-
-        if(this.state.token === "DAI" && this.state.token2 === "TSRD") {
-          this.setState({value2: e.target.value/this.state.rate[1]*this.state.rate[2]}, 
-            () => {console.log(this.state.value2)}
-            )
-        }
-
-        if(this.state.token === "TSRD" && this.state.token2 === "DAI") {
-          this.setState({value2: e.target.value/this.state.rate[2]*this.state.rate[1]}, 
-            () => {console.log(this.state.value2)}
-            )
-        }
-
-        if(this.state.token ===  this.state.token2 && this.state.token!=='Επιλέξτε νόμισμα') {
-          this.setState({value2: e.target.value}, 
-            () => {console.log(this.state.value2)}
-            )
-        }
       }
+
 
      
-
-     onChange2(e){
-      const re = /^[0-9]*([,.][0-9]*)?$/;
-      if (e.target.value === '' || re.test(e.target.value)) {
-         this.setState({value2: e.target.value})
-      }
-
-      if(this.state.token === "ETH" && this.state.token2 === "DAI") {
-        this.setState({value1: e.target.value*this.state.rate[0]/this.state.rate[1]}, 
-          () => {console.log(this.state.value2)}
-          )
-      }
-
-      if(this.state.token === "DAI" && this.state.token2 === "ETH") {
-        this.setState({value1: e.target.value*this.state.rate[1]/this.state.rate[0]}, 
-          () => {console.log(this.state.value2)}
-          )
-      }
-    }  
 
     change(token) {
       this.setState({token:token})
@@ -129,32 +69,33 @@ class Pool extends Component {
         alert("Δεν έχετε συνδέσει κάποιο πορτοφόλι,  \nΘα μεταβείτε στην σελίδα σύνδεσης.")
         ReactDOM.render(<App />, document.getElementById('root'));
       }
-      else if(this.state.token === 'Επιλέξτε νόμισμα' || this.state.token2 === 'Επιλέξτε νόμισμα') {
-        alert("Παρακαλώ επιλέξτε νομίσματα.")
+      else if(this.state.token === 'Επιλέξτε νόμισμα') {
+        alert("Παρακαλώ επιλέξτε νόμισμα.")
       }
-      else if(this.state.value1 === '' || this.state.value2 === '') {
-        alert("Παρακαλώ ορίστε ποσότητα νομισμάτων.")
+      else if(this.state.value1 === ''){
+        alert("Παρακαλώ ορίστε ποσότητα.")
       }
-      else if(isNaN(parseFloat(this.state.value1)) || isNaN(parseFloat(this.state.value2))) {
+      else if(isNaN(parseFloat(this.state.value1))) {
         alert("Οι τιμές δεν συμπληρώθηκαν σωστά.")
       }
      
 
       else {
-        if(this.state.token === 'ETH' && this.state.token2 === 'DAI') {
-          dai.methods.createStake(web3.utils.toWei(String(this.state.value2, 'ether'))).send(
+        if(this.state.token === 'Token1') {this.setState({selectedToken:1})}
+        if(this.state.token === 'Token2') {this.setState({selectedToken:2})}
+        if(this.state.token === 'Token3') {this.setState({selectedToken:3})}
+        {
+          _exchange.methods.createStake(this.state.selectedToken,web3.utils.toWei(String(this.state.value1, 'ether'))).send(
             {
-              from: accounts[0],
-              value: web3.utils.toWei(this.state.value1, 'ether')
+              from: accounts[0]
             }
           );
         }
       }
     }
 
-    showsStake = async () => {
+    withdrawStake = async () => {
 
-      
       const accounts = await web3.eth.getAccounts();
       this.setState ({account: accounts[0]});
 
@@ -165,21 +106,29 @@ class Pool extends Component {
 
       else {
 
-        const _stakes = await dai.methods.stakeOf(accounts[0]).call();
+        const _stakes1 = await _exchange.methods.stakeOf(1, accounts[0]).call(); //καταθέσεις στο Token1
+        const _stakes2 = await _exchange.methods.stakeOf(2, accounts[0]).call(); //καταθέσεις στο Token1
+        const _stakes3 = await _exchange.methods.stakeOf(3, accounts[0]).call(); //καταθέσεις στο Token1
 
-
-        this.setState({stakes: _stakes.toString()/1000000000000000000}); 
+        this.setState({stakes1: _stakes1.toString()/1000000000000000000}); 
+        this.setState({stakes2: _stakes2.toString()/1000000000000000000}); 
+        this.setState({stakes3: _stakes3.toString()/1000000000000000000}); 
         
-        if(this.state.stakes === 0) {alert("Δεν έχετε υπόλοιπο στο pool.")}
+        if(this.state.stakes1 === 0 && this.state.stakes2 === 0 && this.state.stakes3 === 0) 
+          {alert("Δεν έχετε υπόλοιπο στο pool.")}
         else {
-          var answer = window.confirm("Θέλετε να κάνετε ανάληψη "+ this.state.stakes + " dai και " +_stakes/1000000000000000000/100 + " eth;");
+          var answer = window.confirm("Θέλετε να κάνετε ανάληψη "
+          + this.state.stakes1 + 
+          " Token1 και " +
+          this.state.stakes2 +
+           " Token2 και " +
+           this.state.stakes3 +
+           " Token3;");
           if (answer) {
-              dai.methods.removeStake(web3.utils.toWei(String(_stakes/1000000000000000000, 'ether')), web3.utils.toWei(String(_stakes/1000000000000000000/100))).send(
-                {
-                  from: accounts[0]
-                }
-              );
-          }
+              _exchange.methods.removeStake().send({
+                from:accounts[0]
+              })
+            }
           else {
             
           }
@@ -192,21 +141,15 @@ class Pool extends Component {
 
     return (
 
-      <div style={{backgroundColor:'blue'}}>              
+      <div>              
         <Navbar account={this.state.account}/>
-          <div className="container-fluid align-items-center h-100">
-            <div className="align-items-center">
-              <main role="main" className="col-lg-12 d-flex text-center align-items-center h-100">
-                <div className="content mr-auto ml-auto align-items-center h-100" >
+            <div>
+              <main role="main" className="col-lg-12 d-flex text-center align-items-center h-100" style={{paddingTop: 80}}>
+                <div className="content mr-auto ml-auto" >
                   <Card
                     style={{
                       width: 600,
-                      height: 630,
-                      backgroundColor: "white",
-                  
-                      position: 'fixed',
-                      top: '10%',
-                      left: '30%'
+                      height: 500,
                     }}
                   >
                     <Button 
@@ -232,7 +175,7 @@ class Pool extends Component {
                         }}>VOTE</Button>
                     <CardContent>
                       <Typography variant="h4" component="h2">
-                        Pool
+                        Καταθέσεις
                       </Typography>
                       <Typography
                         style={{
@@ -243,7 +186,7 @@ class Pool extends Component {
                         Καταθέστε νομίσματα Eth και Dai!
                       </Typography>
                       <div class="form-group">
-                        <div class="row align-items-center">
+                      <div class="row align-items-center">
                           <div class="col-sm">
                             <input 
                             name= 'input1'
@@ -254,7 +197,7 @@ class Pool extends Component {
                               margin: 20,
                               borderRadius:20,
                               height: 100,
-                              width: 300
+                              width: 300,  
                             }}/> <br/>
                           </div>
                           <div class="col-sm">
@@ -264,47 +207,26 @@ class Pool extends Component {
                                id="dropdown-basic"
                                style={{
                                 width: 160
+                                
                                }}
                                >
-                                <text>Eth</text>
+                                <text>{this.state.token}</text>
                               </Dropdown.Toggle>
                               <Dropdown.Menu>
-                                <Dropdown.Item onSelect={()=> (this.setState({value1: ''}),this.change("ETH"), this.setState({selectedRate1:0}))}><Icon name="eth" size={25} /> ETH </Dropdown.Item>
+                                <Dropdown.Item onSelect={()=> (this.setState({value1: ''}),
+                                  this.change("Token1"), this.setState({selectedRate1:0}))}>
+                                  <Icon name="Token1" size={25} /> Token1 </Dropdown.Item>
+                                <Dropdown.Item onSelect={()=> (this.setState({value1: ''}),
+                                  this.change("Token2"), this.setState({selectedRate1:1}))}>
+                                  <Icon name="Token2" size={25} /> Token2 </Dropdown.Item>
+                                <Dropdown.Item onSelect={()=> (this.setState({value1: ''}),
+                                  this.change("Token3"), this.setState({selectedRate1:2}))}>
+                                  <Icon name="Token3" size={25} /> Token3</Dropdown.Item>
                               </Dropdown.Menu>
                             </Dropdown>
                           </div>
                         </div>
-                        <div class="row align-items-center">
-                          <div class="col-sm">
-                            <input 
-                            name= 'input2'
-                            value={this.state.value2} 
-                            onChange={this.onChange2}
-                            placeholder="0.0"
-                            style={{
-                              margin: 20,
-                              borderRadius:20,
-                              height: 100,
-                              width: 300
-                            }} />
-                          </div>
-                          <div class="col-sm">
-                            <Dropdown>
-                              <Dropdown.Toggle 
-                              variant="success" 
-                              id="dropdown-basic"
-                              style={{
-                                width: 160
-                               }}>
-                              <text>Dai</text>
-                              </Dropdown.Toggle>
-                              <Dropdown.Menu>
-                                <Dropdown.Item onSelect={()=> (this.setState({value2: ''}),this.change2("DAI"), this.setState({selectedRate2:1}))}><Icon name="dai" size={25} /> DAI </Dropdown.Item>
-                              </Dropdown.Menu>
-                            </Dropdown>
-                          </div>
-                        </div>
-                        <h6>Κατάθεση Eth και Dai σε αναλογία 1 προς 100!</h6>
+                      
                       </div>
                      
                     </CardContent>
@@ -320,7 +242,7 @@ class Pool extends Component {
                       </div>
                       <div className="d-grid gap-2" style={{paddingTop: 20}}>
                         <Button 
-                        onClick={this.showsStake}
+                        onClick={this.withdrawStake}
                         variant="outline-info" 
                         size="md"
                         style={{
@@ -332,9 +254,8 @@ class Pool extends Component {
               </div>
             </main>
           </div>
-        </div>
       </div>
     );
     }
 }
-export default Pool;
+export default Pool; 
